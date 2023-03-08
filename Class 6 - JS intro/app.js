8a79c251106b9ac90e8cdce651c268180989f0da
Nicolas Mayorga <namayorgav@gmail.com>
2023-03-07 20:36:36 -0500

    CSS Finished :3

---
+++ Class 6 - JS intro/app.js
@@ -0,0 +1,26 @@
+function sum(a, b) {
+  return a + b
+}
+
+// Arrow functions:
+
+const times = (a, b) => {
+  return a * b
+}
+
+console.log(times(2, 5))
+
+// Anonyms Functions:
+
+let names = ["A", "B", "C"]
+
+names.map(() => {})
+
+/*
+    - array.map()
+    - array.filter()
+    - array.forEach()
+    - array.every()
+    - array.some()
+    - array.reduce()
+*/
